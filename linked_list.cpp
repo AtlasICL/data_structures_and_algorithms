846ca0429f8fbe83198692f28631fcfa17de4a6a
AtlasICL <emreacarsoy09@gmail.com>
2025-03-04 22:19:28 +0000

    changed template typename from list_t to T

--- linked_list.cpp
+++ linked_list.cpp
@@ -9,12 +9,12 @@
 namespace atlas
 {
 
-template<typename list_t>
+template<typename T>
 struct ListNode{
-    list_t val;
-    ListNode<list_t>* nextNode;
+    T val;
+    ListNode<T>* nextNode;
 
-    ListNode(list_t v, ListNode<list_t>* next) : val(v), nextNode(next) { }
+    ListNode(T v, ListNode<T>* next) : val(v), nextNode(next) { }
 
     // explicit conversion operator to convert a ListNode object to a string
     // debugging function, produces output of type [ListNode | addr: 0xffff val: {value}]
@@ -25,23 +25,23 @@ struct ListNode{
     }
 };
 
-template<typename list_t>
+template<typename T>
 class LinkedList{
 private:
-    ListNode<list_t>* head;
-    ListNode<list_t>* tail;
+    ListNode<T>* head;
+    ListNode<T>* tail;
 
 public:
     LinkedList() : head(nullptr), tail(nullptr) { }
 
-    LinkedList(const std::vector<list_t>& values){
+    LinkedList(const std::vector<T>& values){
         this->createList(values);
     }
 
     // destructor should deallocate all memory used by the list
     ~LinkedList(){
-        ListNode<list_t>* current = head;
-        ListNode<list_t>* tmp;
+        ListNode<T>* current = head;
+        ListNode<T>* tmp;
 
         while (current != nullptr){
             tmp = current->nextNode;
@@ -52,7 +52,7 @@ public:
 
     // copy constructor - we want to implement deep copy instead of shallow copy
     LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr) {
-        for (ListNode<list_t>* current = other.head; current != nullptr; current = current->nextNode){
+        for (ListNode<T>* current = other.head; current != nullptr; current = current->nextNode){
             insertAtTail(current->val);
         }
     }
@@ -69,12 +69,12 @@ public:
 
 
     // this function creates a linked list with the values provided by the vector argument
-    void createList(const std::vector<list_t>& values){
+    void createList(const std::vector<T>& values){
         head = nullptr;
         tail = nullptr;
 
-        for (const list_t& value : values){
-            ListNode<list_t>* newNode = new ListNode<list_t>(value, nullptr);
+        for (const T& value : values){
+            ListNode<T>* newNode = new ListNode<T>(value, nullptr);
 
             // if the list is empty, this created new node becomes both the head and the tail
             // as it is the only node in the list
@@ -93,7 +93,7 @@ public:
     // this function returns the number of nodes in the linked list
     int countNodes() const {
         int nodeCount = 0;
-        ListNode<list_t>* ptr = head;
+        ListNode<T>* ptr = head;
         while (ptr != nullptr) {
             nodeCount++;
             ptr = ptr->nextNode;
@@ -102,25 +102,25 @@ public:
     }
 
     // this function inserts a node at the head of the linked list
-    void insertAtHead(const list_t v){
+    void insertAtHead(const T v){
         // if the list is empty, we set both head and tail to be this new node
         if (head == nullptr){
-            ListNode<list_t>* tmp = new ListNode<list_t>(v, nullptr);
+            ListNode<T>* tmp = new ListNode<T>(v, nullptr);
             this->head = tmp;
             this->tail = tmp;
             return;
         }
 
         // otherwise we add a new node, which has next = head, then we update head
-        ListNode<list_t>* tmp = new ListNode<list_t>(v, head);
+        ListNode<T>* tmp = new ListNode<T>(v, head);
         this->head = tmp;
     }
 
     // this function inserts a node at the tail of the linked list
-    void insertAtTail(const list_t v){
+    void insertAtTail(const T v){
         // if the list is empty, we set both head and tail to be this new node
         if (head == nullptr){
-            ListNode<list_t>* tmp = new ListNode<list_t>(v, nullptr);
+            ListNode<T>* tmp = new ListNode<T>(v, nullptr);
             this->head = tmp;
             this->tail = tmp;
             return;
@@ -128,13 +128,13 @@ public:
 
         // otherwise we add a new node, which has next = nullptr, as it as the tail
         // we link the new node to the previous tail, then we update tail
-        ListNode<list_t>* tmp = new ListNode<list_t>(v, nullptr);
+        ListNode<T>* tmp = new ListNode<T>(v, nullptr);
         this->tail->nextNode = tmp;
         this->tail = tmp;
     }
 
     // this function inserts a node at a specified index of the linked list
-    void insertAtIndex(const list_t v, const int idx){
+    void insertAtIndex(const T v, const int idx){
         // check provided index is positive
         if (idx < 0) {
             throw std::invalid_argument("Negative index not allowed.");
@@ -154,12 +154,12 @@ public:
         }
 
         // traverse to the node at idx
-        ListNode<list_t>* current = head;
+        ListNode<T>* current = head;
         for (int i = 0; i < idx - 1; ++i) {
             current = current->nextNode;
         }
 
-        ListNode<list_t>* tmp = new ListNode<list_t>(v, current->nextNode);
+        ListNode<T>* tmp = new ListNode<T>(v, current->nextNode);
         current->nextNode = tmp;
         
         // if idx was the last element, update tail
@@ -170,13 +170,13 @@ public:
 
     // this function returns true if the linked list contains a node with the value v
     // false otherwise
-    bool contains(const list_t v) const {
+    bool contains(const T v) const {
         if (head == nullptr){
             // the list is empty, and therefore definitely does not contain the value v
             return false;
         }
 
-        ListNode<list_t>* current = head;
+        ListNode<T>* current = head;
 
         // iterate through the nodes
         while (current != nullptr){
@@ -190,7 +190,7 @@ public:
 
     // this function deletes the first instance of a value in the linked list
     // returns true if a deletion occurred, else false (the value was not found in the list)
-    bool deleteFirstInstance(const list_t v){
+    bool deleteFirstInstance(const T v){
         
         // if the list is empty, we do not delete anything, hence return false
         if (head == nullptr){
@@ -199,7 +199,7 @@ public:
 
         // check the case where head contains the target value
         if (head->val == v){
-            ListNode<list_t>* tmp = head;
+            ListNode<T>* tmp = head;
             head = head->nextNode;
 
             if (head == nullptr){
@@ -213,10 +213,10 @@ public:
         }
 
         // traverse the list
-        ListNode<list_t>* current = head;
+        ListNode<T>* current = head;
         while (current->nextNode != nullptr){
             if (current->nextNode->val == v){
-                ListNode<list_t>* tmp = current->nextNode;
+                ListNode<T>* tmp = current->nextNode;
                 current->nextNode = current->nextNode->nextNode;
 
                 if (tmp == tail) {
@@ -236,16 +236,16 @@ public:
     }
 
     // this function deletes all instances of a value in the linked list
-    void deleteAllInstances(const list_t v){
+    void deleteAllInstances(const T v){
         while (deleteFirstInstance(v)){} // delete the first instance of v repeatedly, until it returns false
         // this implementation is not particularly efficient, as we traverse the list multiple times
         // this could be implemented with only one travesal of the list.
     }
 
     // this function returns the number of the nodes with the specified value
-    int countInstances(const list_t v) const {
+    int countInstances(const T v) const {
         int counter = 0; // counts the number of occurrences of v
-        ListNode<list_t>* current = head;
+        ListNode<T>* current = head;
 
         // traverse the list
         while (current != nullptr){
@@ -258,11 +258,11 @@ public:
 
     // this function reverses the order of the linked list in-place
     void reverse(){
-        ListNode<list_t>* previous = nullptr;
-        ListNode<list_t>* current = this->head;
+        ListNode<T>* previous = nullptr;
+        ListNode<T>* current = this->head;
 
         while (current != nullptr){
-            ListNode<list_t>* tmp = current->nextNode;  // save the next node
+            ListNode<T>* tmp = current->nextNode;  // save the next node
             current->nextNode = previous;       // reversal, current->next now points to its preceding node
             previous = current;                 // previous becomes current
             current = tmp;                      // current becomes next node
@@ -281,7 +281,7 @@ public:
         }
         
         // iterate through each node in the list
-        for (ListNode<list_t>* current = head; current != nullptr; current = current->nextNode) {
+        for (ListNode<T>* current = head; current != nullptr; current = current->nextNode) {
             // using the explicit conversion operator to convert the node to a string
             std::cout << static_cast<std::string>(*current);
         }
