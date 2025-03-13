48f1b6a5bca7facb6a5d28a081cff9c706b13547
AtlasICL <emreacarsoy09@gmail.com>
2025-03-13 17:24:03 +0000

    formatting - bracket spacing

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -3,18 +3,16 @@
 #include <sstream>
 #include <stdexcept>
 
-namespace atlas
-{
 
 template<typename T>
-struct QueueNode{
+struct QueueNode {
 
     T val;
     QueueNode* next;
 
-    QueueNode() : val(), next(nullptr) { }
-    QueueNode(T val) : val(val), next(nullptr) { }
-    QueueNode(T val, QueueNode<T>* next) : val(val), next(next) { }
+    QueueNode() : val(), next(nullptr) {}
+    QueueNode(T val) : val(val), next(nullptr) {}
+    QueueNode(T val, QueueNode<T>* next) : val(val), next(next) {}
 
     // Explicit conversion operator to convert from QueueNode object to a string.
     // Output of type [ListNode | addr: 0xffff val: {value}].
@@ -33,10 +31,10 @@ private:
     QueueNode<T>* tail;
 
 public:
-    Queue() : head(nullptr), tail(nullptr) { }
+    Queue() : head(nullptr), tail(nullptr) {}
 
     ~Queue(){
-        while (head != nullptr){
+        while (head != nullptr) {
             QueueNode<T>* tmp = head->next;
             delete head;
             head = tmp;
@@ -63,7 +61,7 @@ public:
 
     void enqueue(T val){
         QueueNode<T>* tmp = new QueueNode<T>(val);
-        if (tail == nullptr){ // Queue is empty
+        if (tail == nullptr) { // Queue is empty
             head = tmp;
             tail = tmp;
         }
@@ -74,13 +72,13 @@ public:
     }
 
     T dequeue(){
-        if (head == nullptr){
+        if (head == nullptr) {
             throw std::out_of_range("Called dequeue on empty queue");
         }
         QueueNode<T>* tmp = head;
         T value = head->val;
         head = head->next;
-        if (head == nullptr){ // Queue becomes empty
+        if (head == nullptr) { // Queue becomes empty
             tail = nullptr;
         }
         delete tmp;
@@ -88,12 +86,12 @@ public:
     }
 
     int size() const {
-        if (head == nullptr){
+        if (head == nullptr) {
             return 0;
         }
         int counter = 0;
         QueueNode<T>* curr = head;
-        while (curr != nullptr){
+        while (curr != nullptr) {
             curr = curr->next;
             counter++;
         }
@@ -103,7 +101,7 @@ public:
     void print() const {
         QueueNode<T>* curr = head;
         std::cout << "Queue{\n";
-        while (curr != nullptr){
+        while (curr != nullptr) {
             std::cout << "    " << static_cast<std::string>(*curr);
             curr = curr->next;
         }
@@ -117,20 +115,4 @@ public:
     }
 
 };
-
-} // namespace atlas
-
-
-
-int main(){
-    atlas::Queue<int> myQ;
-
-    myQ.enqueue(2);
-    myQ.enqueue(6);
-    myQ.enqueue(5);
-    myQ.enqueue(3);
-    myQ.enqueue(9);
-
-    myQ.print();
-}
 
