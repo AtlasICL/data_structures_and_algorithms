d9e139e237ad2f445275f497f6f5f3b46e6f22fd
AtlasICL <emreacarsoy09@gmail.com>
2025-03-14 00:13:29 +0000

    added week9 instructions

---
+++ weekly_problems/week9.cpp
@@ -0,0 +1,13 @@
+/**
+ * Problem Week 9:
+ * 
+ * Implement a binary tree with functions for insertion and inorder traversal.
+ * 
+ * Challenge: implement an AVL tree (or red-black tree) with insertion and
+ * rotation operations. 
+ * Test it with a series of input values to demonstrate self-balancing properties.
+ */
+
+#include <iostream>
+#include <stdexcept>
+
