c71d4cae98f425a8f6aca5d5ff18632ce943d0fd
AtlasICL <emreacarsoy09@gmail.com>
2025-03-16 14:34:51 +0000

    more progress on AVL

--- trees/AVL_tree.cpp
+++ trees/AVL_tree.cpp
@@ -44,13 +44,43 @@ private:
 
 public:
     AVLTree() : root(nullptr) {}
+    bool contains(T val);
     Node<T>* leftRotate(Node<T>* x);
     Node<T>* rightRotate(Node<T>* y);
     Node<T>* insert(Node<T>* node, T val);
     void insert(T val); // interface to insert a value
+    void deleteVal(T val);
     void inorder(Node<T>* node);
 
-    
+
+    Node<T>* insert(Node<T>* node, T val) const {
+
+        if (contains(val)) {
+            throw std::invalid_argument("Attempted to insert duplicate element");
+        }
+
+        // 1. standard BST insertion
+        if (node == nullptr) {
+            return new Node<T>(val);
+        }
+
+        if (val < node->val) {
+            node->left = insert(node->left, val);
+        }
+        else if (val > node->val) {
+            node->right = insert(node->right, val);
+        }
+        
+        // 2. update height of the parent node
+        node->height = max(Node<T>::getHeight(node->left), Node<T>::getHeight(node->right)) + 1;
+
+        // 3. check if this node has become unbalanced
+        int balance = Node<T>::getBalance(node);
+
+        // 4. if it is unbalanced, 4 possible cases
+
+
+    }
 
 
 };
