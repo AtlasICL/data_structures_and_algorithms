d623f0fa06cf41803742049921ed15d8ecc2d732
AtlasICL <emreacarsoy09@gmail.com>
2025-03-30 15:57:35 +0100

    completed week 2 task

---
+++ weekly_problems/week2/week2.py
@@ -0,0 +1,31 @@
+# Comparison of number of operations in a
+# sequantial search over an unsorted array
+
+import random
+
+array_to_search: list[int] = [random.choice(range(1, 250)) for _ in range(100)]
+
+numbers_to_search_for: list[int] = [random.choice(array_to_search) for _ in range(3)]
+
+def search_and_count(array: list[int], n: int) -> int:
+    """
+    Searches for element with value n in array.
+    Returns the number of operations to find n.
+    """
+    comparison_counter: int = 0
+    for element in array:
+        comparison_counter += 1
+        if element == n:
+            return comparison_counter
+    return -1
+
+
+def main():
+    for num in numbers_to_search_for:
+        print(f"-- Searching for {num} --")
+        ops: int = search_and_count(array_to_search, num)
+        print(f"Found in {ops} operations")
+
+
+if __name__ == "__main__":
+    main()
