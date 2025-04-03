8fa30632e14f590adc84546d9dedf439a17e7254
AtlasICL <emreacarsoy09@gmail.com>
2025-04-03 15:34:53 +0100

    basic implementation of directed graph

--- graphs/directed_graph.cpp
+++ graphs/directed_graph.cpp
@@ -4,3 +4,52 @@
 #include <string>
 #include <sstream>
 #include <stdexcept>
+
+class DirectedGraph {
+    private:
+    int V; // number of vertices
+    std::vector<std::vector<int>> adj; // adjacency list
+
+public:
+    DirectedGraph() : V(0), adj(0) {}
+    DirectedGraph(int V) : V(V), adj(V) {}
+
+    // add an edge to the graph, connecting u to v (order matters!)
+    // @throws out_of_range if u or v are not valid vertices
+    void addEdge(int u, int v) {
+        if (u >= V || v >= V || u < 0 || v < 0) {
+            throw std::out_of_range("Vertex index out of bounds");
+        }
+        adj[u].push_back(v);
+    }
+
+    // displays the node count and the adjacency list (neighbours) of each node
+    void printGraph() const {
+        std::cout << "***** DIRECTED GRAPH *****" << std::endl;
+        std::cout << "---- Vertex count = " << V << " ----" << std::endl;
+        for (int i = 0; i < V; i++) {
+            std::cout << "| " << i << ": ";
+            for (const auto& neighbor : adj[i]) {
+                std::cout << neighbor << " ";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "--------------------------" << std::endl;
+    }
+};
+
+int main() {
+
+    DirectedGraph g(5);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+    g.addEdge(3, 0);
+
+    g.printGraph();
+}
