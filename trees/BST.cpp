719da7b6745c846d01b7a366f70a11d3997d26c3
AtlasICL <emreacarsoy09@gmail.com>
2025-04-06 02:11:00 +0100

    BST

--- trees/BST.cpp
+++ trees/BST.cpp
@@ -6,77 +6,133 @@ Implementation of a Binary Search Tree.
 #include <vector>
 #include <stdexcept>
 
-typedef int T;
-
-struct TreeNode {
+template<typename T>
+struct Node {
     T val;
-    TreeNode* left;
-    TreeNode* right;
+    Node* left;
+    Node* right;
 
-    TreeNode() : val(), left(nullptr), right(nullptr) {}
-    TreeNode(T val) : val(val), left(nullptr), right(nullptr) {}
-    TreeNode(T val, TreeNode* left, TreeNode* right) : val(val), left(left), right(right) {}
+    Node() : val(), left(nullptr), right(nullptr) {}
+    Node(T val) : val(val), left(nullptr), right(nullptr) {}
+    Node(T val, Node* left, Node* right) 
+        : val(val), left(left), right(right) {}
 };
 
-
+template<typename T>
 class BST {
-private: 
-    TreeNode* root;
+private:
+    Node<T>* m_root; // pointer to root node
 
 public:
+    BST() : m_root(nullptr) {}
 
-    // inserts a node with the specified value in the appropriate location
-    // if provided value already exists in the BST, nothing will be changed
-    void insertNode(T val) {
-        if (contains(val)) { // BST already contains provided value; do nothing
-            return; 
-        }
+    BST(T val) : m_root(new Node<T>(val)) {}
 
-        if (root == nullptr) { // tree is empty, insert at root
-            root = new TreeNode(val);
-            return;
-        }
-
-        TreeNode* curr = root;
-        TreeNode* parent = nullptr;
-
-        while (curr != nullptr) {
-            parent = curr;
-            if (val < curr->val) {
-                curr = curr->left;
-            }
-            else if (val > curr->val) {
-                curr = curr->right;
-            }
-        }
-        
-        if (val < parent->val) {
-            parent->left = new TreeNode(val);
-        }
-        else if (val > parent->val) {
-            parent->right = new TreeNode(val);
-        }
-
-    } 
-
-    bool contains(T val) {
-        if (root == nullptr) { // tree is empty
-            return false;
-        }
-        TreeNode* curr = root;
-        do {
-            if (curr->val == val) {
-                return true;
-            }
-            if (val < curr->val) {
-                curr = curr->left;
-            }
-            else if (val > curr->val) {
-                curr = curr->right;
-            }
-        } while (curr != nullptr);
-        
-        return false;
+    ~BST() {
+        dealloc(m_root);
     }
 
+    // inserts the given value in the BST
+    // @throws invalid_argument if val already exists in the tree
+    void insert(const T& val) {
+        if (contains(val)) { throw std::invalid_argument("Attempted to insert duplicate element"); }
+        m_root = insert(val, m_root);
+    }
+
+    // @returns Returns true if the tree contains the value val
+    // @returns false otherwise.
+    bool contains(const T& val) const {
+        return contains(val, m_root);
+    }
+
+    // performs a pre-order depth-first traversal of the tree
+    // and prints out the order in which the nodes are visited
+    // NOTE: this implementation is called pre-order because it processes 
+    // the root node before the subtrees
+    void pre_order_DFS() const {
+        std::cout << "**** Performing pre-order DFS ****" << std::endl;
+        pre_order_DFS(m_root);
+        std::cout << std::endl;
+    }
+
+    // performs an in-order depth-first traversal of the tree
+    // and prints out the order in which the nodes were visited
+    void in_order_DFS() const {
+        std::cout << "**** Performing in-order DFS ****" << std::endl;
+        in_order_DFS(m_root);
+        std::cout << std::endl;
+    }
+    
+private:
+    // helper function to free the memory used by the tree 
+    void dealloc(Node<T>* ptr) {
+        if (ptr == nullptr) { return; }
+        dealloc(ptr->left);
+        dealloc(ptr->right);
+        delete ptr;
+    }
+
+    // helper function for recursion
+    // checks if the subtree with root curr_node contains the value val
+    bool contains(const T& val, Node<T>* curr_node) const {
+        if (curr_node == nullptr) {
+            return false;
+        }
+        if (curr_node->val == val) {
+            return true;
+        }
+        return val < curr_node->val ? contains(val, curr_node->left) : contains(val, curr_node->right);
+    }
+
+    // recursive helper for insertion
+    Node<T>* insert(const T& val, Node<T>* curr_root) {
+        if (curr_root == nullptr) {
+            return new Node<T>(val);
+        }
+        if (val <= curr_root->val) {
+            curr_root->left = insert(val, curr_root->left);
+        }
+        else if (val > curr_root->val) {
+            curr_root->right = insert(val, curr_root->right);
+        }
+        return curr_root;
+    }
+
+    // helper function for pre-order DFS
+    void pre_order_DFS(Node<T>* curr_node) const {
+        if (curr_node == nullptr) {
+            return;
+        }
+        std::cout << "| " << curr_node->val << " ";
+        pre_order_DFS(curr_node->left);
+        pre_order_DFS(curr_node->right);
+    }
+
+    // helper function for in-order DFS
+    void in_order_DFS(Node<T>* curr_node) const {
+        if (curr_node == nullptr) {
+            return;
+        }
+        in_order_DFS(curr_node->left);
+        std::cout << "| " << curr_node->val << " ";
+        in_order_DFS(curr_node->right);
+    }
 };
+
+int main() {
+
+    BST<int> t(6);
+    t.insert(2);
+    t.insert(7);
+    t.insert(1);
+    t.insert(4);
+    t.insert(3);
+    t.insert(5);
+    t.insert(9);
+    t.insert(8);
+
+    t.pre_order_DFS();
+
+    t.in_order_DFS();
+
+}
