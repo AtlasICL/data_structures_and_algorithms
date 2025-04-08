711f445a5f2b3f622e2f87f32d63dc1cb47c5955
AtlasICL <emreacarsoy09@gmail.com>
2025-04-08 15:22:12 +0100

    refactored rods into enum

--- weekly_problems/week4.cpp
+++ weekly_problems/week4.cpp
@@ -6,9 +6,26 @@
 #include <string>
 #include <stdexcept>
 
+enum Rod {
+    A,
+    B,
+    C
+}; 
+
+// helper function to convert Rod enum to string
+std::string rodToStr(Rod rod) {
+    switch (rod) {
+        case Rod::A: return "A";
+        case Rod::B: return "B";
+        case Rod::C: return "C";
+        default: return "";
+    }
+}
+
+
 class HanoiSolver {
 public:
-    void solve(int num_disks, std::string source, std::string aux, std::string target) {
+    void solve(int num_disks, Rod source, Rod aux, Rod target) {
         if (num_disks <= 0) {
             std::invalid_argument("Invalid number of disks for Hanoi");
         }
@@ -16,7 +33,7 @@ public:
     }
 
 private:
-    void moveDisks(int num_disks, std::string source, std::string aux, std::string target) {
+    void moveDisks(int num_disks, Rod source, Rod aux, Rod target) {
         if (num_disks == 1) { // base case
             printMove(1, source, target);
             return;
@@ -27,14 +44,13 @@ private:
     }
 
     // prints a move to the console
-    void printMove(int diskNumber, std::string sourceRod, std::string destinationRod) const {
-        std::cout << "Move disk " << diskNumber << " from " << sourceRod << " to " << destinationRod << std::endl;
+    void printMove(int diskNumber, Rod sourceRod, Rod destinationRod) const {
+        std::cout << "Move disk " << diskNumber << " from " << rodToStr(sourceRod) << " to " << rodToStr(destinationRod) << std::endl;
     }
-
 };
 
 int main() {
-    int NUM_DISKS = 5;
+    int NUM_DISKS = 2;
     HanoiSolver solver;
-    solver.solve(NUM_DISKS, "A", "B", "C");
+    solver.solve(NUM_DISKS, Rod::A, Rod::B, Rod::C);
 }
