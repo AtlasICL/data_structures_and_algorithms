8459edddf584c8130362a0154956f787f60fb4e7
AtlasICL <emreacarsoy09@gmail.com>
2025-04-12 19:39:06 +0100

    implemented Floyd-Warshall algorithm

--- data_structures/graphs/14_floyd_warshall_algorithm.cpp
+++ data_structures/graphs/14_floyd_warshall_algorithm.cpp
@@ -25,38 +25,18 @@ private:
     int V; // vertex count
     std::vector<Edge> edges; // edges
 
-    inline bool isValidVertex(int u) const {
+    bool isValidVertex(int u) const {
         return u >= 0 && u < V;
     }
 
-    inline bool isValidVertex(int u, int v) const {
+    bool isValidVertex(int u, int v) const {
         return isValidVertex(u) && isValidVertex(v);
-    }
-
-public:
-    Graph(int V) : V(V) {}
-
-    int getVertexCount() const {
-        return V;
-    }
-
-    std::vector<Edge> getEdges() const {
-        return edges;
-    }
-
-    // adds an edge to the graph, with given edge weight
-    // @throws invalid_argument if u or v are not valid nodes
-    void addEdge(int u, int v, int weight) {
-        if (u < 0 || u >= V || v < 0 || v >= V) {
-            throw std::invalid_argument("Invalid vertex index");
-        }
-        edges.push_back({u, v, weight});
     }
 
     // Returns true if the nodes u and v are directly connected via an edge,
     // or if u and v are the same node.
     // Returns false otherwise.
-    bool edgeConnects(int u, int v) {
+    bool edgeConnects(int u, int v) const {
         if (!isValidVertex(u, v)) {
             throw std::invalid_argument("Invalid vertex index");
         }
@@ -74,7 +54,7 @@ public:
     // Returns the weight of the edge connecting nodes u and v,
     // and INF if the two nodes are not directly linked by one edge.
     // If u and v are the same node, the weight returned is 0.
-    int edgeWeight(int u, int v) {
+    int edgeWeight(int u, int v) const {
         if (u == v) {
             return 0;
         }
@@ -82,10 +62,60 @@ public:
             return INF;
         }
         for (const Edge& edge : edges) {
-            if (edge.u == u && edge.v == v || edge.u == v && edge.v == u) {
+            if ((edge.u == u && edge.v == v) || (edge.u == v && edge.v == u)) {
                 return edge.weight;
             }
         }
+        return INF;
+    }
+
+public:
+    Graph(int V) : V(V) {}
+
+    // adds an edge to the graph, with given edge weight
+    // @throws invalid_argument if u or v are not valid nodes
+    void addEdge(int u, int v, int weight) {
+        if (u < 0 || u >= V || v < 0 || v >= V) {
+            throw std::invalid_argument("Invalid vertex index");
+        }
+        edges.push_back({u, v, weight});
+    }
+
+    // Implements the Floyd-Warshall algorithm.
+    // Returns a V*V-sized matrix, with the element at (i, j) being the shortest
+    // distance between nodes i and j.
+    // If the graph is undirected, this will be a symmetric matrix.
+    std::vector<std::vector<int>> shortestDistanceMatrix() const {
+
+        std::vector<std::vector<int>> dist(V, std::vector<int>(V)); // initialise a V * V matrix 
+        
+        // set up the distance matrix with dist(y, x) = edgeWeight(y, x) if such an edge exists,
+        // and INF otherwise
+        for (int y = 0 ; y < V ; y++) {
+            for (int x = 0 ; x < V ; x++) {
+                if (y == x) {
+                    dist[y][x] = 0; 
+                }
+                else if (!edgeConnects(y, x)) {
+                    dist[y][x] = INF;
+                }
+                else {
+                    dist[y][x] = edgeWeight(y, x);
+                }
+            }
+        }
+
+        // Consider each node k as a potential intermediary node.
+        // If dist[y][k] + dist[k][x] is shorter than dist [y][x], we update dist[y][x] (passing
+        // through the intermediary node k is the new shortest path between y and x).
+        for (int k = 0 ; k < V ; k++) {
+            for (int y = 0 ; y < V ; y++) {
+                for (int x = 0 ; x < V ; x++) {
+                    dist[y][x] = std::min(dist[y][x], dist[y][k] + dist[k][x]);
+                }
+            }
+        }
+        return dist;
     }
 
 };
