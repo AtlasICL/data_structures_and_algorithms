7637e1ae642a88b08f3e50377231ed5c7ff2f71b
AtlasICL <emreacarsoy09@gmail.com>
2025-04-15 17:15:26 +0100

    restart Prim's

--- algorithms/graphs/prims_algorithm.cpp
+++ algorithms/graphs/prims_algorithm.cpp
@@ -1,42 +1,9 @@
 /**
- * Implementation of Prim's algorithm for finding
- * the MST of a weighted undirected graph.
- * -- INCOMPLETE --
+ * Implementation of Prim's algorithm, to find the MST of
+ * a weighted undirected graph.
+ * 
+ * The graph implementation used here is a weighted undirected
+ * graph implement using an adjacency list (and not an edge list).
+ * Both versions can be found in the data_structures folder.
  */
 
-#include <vector>
-#include <iostream>
-#include <stdexcept>
-#include <algorithm>
-
-// struct to represent an edge in the graph
-struct Edge {
-    int u;
-    int v;
-    int weight;
-};
-
-// Graph class for a weighted undirected graph, using an edge list
-class Graph {
-private:
-    int V; // vertex count
-    std::vector<Edge> edges; // edges
-
-public:
-    Graph(int V) : V(V) {}
-
-    // adds an edge to the graph, with given edge weight
-    // @throws invalid_argument if u or v are not valid nodes
-    void addEdge(int u, int v, int weight) {
-        if (u < 0 || u >= V || v < 0 || v >= V) {
-            throw std::invalid_argument("Invalid vertex index");
-        }
-        edges.push_back({u, v, weight});
-    }
-
-    // finds the MST of the graph using Prim's algorithm
-
-    // TODO: actually do the work
-
-    // apparently Prim's works best with an adjacency list
-};
