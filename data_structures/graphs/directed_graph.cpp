bc7e8918dbbf9853e25f2421d89a67953fb6074c
AtlasICL <ea616@exeter.ac.uk>
2025-10-20 21:46:24 +0100

    Commented out main

--- data_structures/graphs/directed_graph.cpp
+++ data_structures/graphs/directed_graph.cpp
@@ -41,18 +41,18 @@ public:
     }
 };
 
-int main() {
+// int main() {
 
-    DirectedGraph g(5);
+//     DirectedGraph g(5);
 
-    g.addEdge(0, 1);
-    g.addEdge(0, 4);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.addEdge(3, 0);
+//     g.addEdge(0, 1);
+//     g.addEdge(0, 4);
+//     g.addEdge(1, 2);
+//     g.addEdge(1, 3);
+//     g.addEdge(1, 4);
+//     g.addEdge(2, 3);
+//     g.addEdge(3, 4);
+//     g.addEdge(3, 0);
 
-    g.printGraph();
-}
+//     g.printGraph();
+// }
--- data_structures/graphs/undirected_graph.cpp
+++ data_structures/graphs/undirected_graph.cpp
@@ -59,19 +59,19 @@ public:
 };
 
 
-int main() {
+// int main() {
 
-    UndirectedGraph g(5);
+//     UndirectedGraph g(5);
 
-    g.addEdge(0, 1);
-    g.addEdge(0, 4);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.addEdge(3, 0);
+//     g.addEdge(0, 1);
+//     g.addEdge(0, 4);
+//     g.addEdge(1, 2);
+//     g.addEdge(1, 3);
+//     g.addEdge(1, 4);
+//     g.addEdge(2, 3);
+//     g.addEdge(3, 4);
+//     g.addEdge(3, 0);
 
-    g.printGraph();
+//     g.printGraph();
     
-}
+// }
