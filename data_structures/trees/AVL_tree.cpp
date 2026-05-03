2309b4c319ef8886e8d9098c37ca7b4ad31c773f
Emre Acarsoy <dev@eacarsoy.com>
2026-05-03 17:41:39 +0100

    [feat] add DFS/BFS logic to AVL tree

--- data_structures/trees/AVL_tree.cpp
+++ data_structures/trees/AVL_tree.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <cstdint>
 #include <vector>
+#include <queue>
+#include <stack>
 
 template<typename T>
 class AVLTree {
@@ -137,8 +139,7 @@ private:
         else if (val > node->val) {
             node->right = insert(val, node->right);
         }
-        else if (val == node->val) {
-            // val already exists
+        else { // val already exists
             return node;
         }
         return balance(node);
@@ -160,26 +161,16 @@ private:
      * Helper function for recursion.
      * Checks if the subtree with root curr_node contains the value val.
      */
-    bool contains(const T& val, Node* curr_node) const {
+    static bool contains(const T& val, Node* curr_node) {
         if (curr_node == nullptr) { return false; }
         if (curr_node->val == val) { return true; }
         return val < curr_node->val ? contains(val, curr_node->left) : contains(val, curr_node->right);
     }
 
     /**
-     * Helper function for in-order traversal. 
-     */ 
-    static void in_order_DFS(Node* curr_node) {
-        if (curr_node == nullptr) { return; }
-        in_order_DFS(curr_node->left);
-        std::cout << "| " << curr_node->val << " ";
-        in_order_DFS(curr_node->right);
-    }
-
-    /**
      * Helper function for removing elements.
      */ 
-    Node* remove(const T& val, Node* root) {
+    static Node* remove(const T& val, Node* root) {
         if (root == nullptr) { throw std::invalid_argument("val not found"); }
 
         if (val < root->val) {
@@ -192,13 +183,13 @@ private:
             Node* left_child = root->left;
             Node* right_child = root->right;
 
-            // if the node had no children
+            // If the node had no children:
             if (left_child == nullptr && right_child == nullptr) {
                 delete root;
                 return nullptr;
             }
 
-            // if the node had only one child:
+            // If the node had only one child:
             if (left_child == nullptr) {
                 delete root;
                 return right_child;
@@ -208,8 +199,8 @@ private:
                 return left_child;
             }
 
-            // if the node had two children, we need to find which one to connect to the grandparent
-            // the node which must be connected is the in-order successor (smallest node in the right subtree)
+            // If the node had two children, we need to find which one to connect to the grandparent.
+            // The node which must be connected is the in-order successor (smallest node in the right subtree).
             Node* tmp = inorderSuccessor(root);
             root->val = tmp->val;
             root->right = remove(tmp->val, root->right); // we call the remove function with the val of
@@ -220,8 +211,10 @@ private:
         return balance(root);
     }
 
-    // helper function to find the in-order successor of a node
-    // @returns Returns the address of the successor (not the value!)
+    /**
+     * @brief Helper function to find the in-order successor of a node
+     * @returns Returns the address of the successor (not the value!)
+     */
     static Node* inorderSuccessor(Node* node) {
         if (node == nullptr) {
             return nullptr;
@@ -238,12 +231,15 @@ public:
     
     explicit AVLTree(const T& val) : m_root(new Node(val)) {}
     
-    ~AVLTree() {
-        dealloc(m_root);
-    }
+    ~AVLTree() { dealloc(m_root); }
+
+    // Explicitly delete copy and move constructors.
+    AVLTree(const AVLTree&) = delete;
+    AVLTree& operator=(const AVLTree&) = delete;
 
     /**
      * @brief Inserts a value into the tree.
+     * @note No-op if val already exists.
      */
     void insert(const T& val) {
         m_root = insert(val, m_root);
@@ -265,4 +261,91 @@ public:
     bool contains(const T& val) const {
         return contains(val, m_root);
     }
+
+    /**
+     * @brief Inorder DF traversal of the tree.
+     * @returns Returns a vector with the elements in the traversed order.
+     */
+    std::vector<T> inOrderTraversal() const {
+        std::vector<T> out;
+        std::stack<Node*> stack;
+        Node* curr = m_root;
+        while (curr != nullptr || !stack.empty()) {
+            while (curr != nullptr) {
+                stack.push(curr);
+                curr = curr->left;
+            }
+            curr = stack.top(); stack.pop();
+            out.push_back(curr->val);
+            curr = curr->right;
+        }
+        return out;
+    }
+
+    /**
+     * @brief Preorder DF traversal of the tree.
+     * @returns Returns a vector with the elements in the traversed order.
+     */
+    std::vector<T> preOrderTraversal() const {
+        std::vector<T> out;
+        if (m_root == nullptr) { return out; }
+
+        std::stack<Node*> stack;
+        stack.push(m_root);
+
+        while (!stack.empty()) {
+            Node* curr = stack.top(); stack.pop();
+            out.push_back(curr->val);
+            if (curr->right != nullptr) { stack.push(curr->right); }
+            if (curr->left != nullptr)  { stack.push(curr->left); }
+        }
+        return out;
+    }
+
+    /**
+     * @brief Postorder DF traversal of the tree.
+     * @returns Returns a vector with the elements in the traversed order.
+     */
+    std::vector<T> postOrderTraversal() const {
+        std::vector<T> out;
+        if (m_root == nullptr) { return out; }
+
+        std::stack<Node*> stack;
+        stack.push(m_root);
+
+        while (!stack.empty()) {
+            Node* curr = stack.top(); stack.pop();
+            out.push_back(curr->val);
+            if (curr->left != nullptr)  { stack.push(curr->left); }
+            if (curr->right != nullptr) { stack.push(curr->right); }
+        }
+
+        std::reverse(out.begin(), out.end());
+        return out;
+    }
+
+    /**
+     * @brief BF traversal of the tree.
+     * @returns Returns a vector with the elements in the traversed order.
+     */
+    std::vector<T> breadthFirstTraversal() const {
+        std::queue<Node*> queue; 
+        std::vector<T> output_order = {};
+
+        if (m_root == nullptr) { return {}; }
+
+        queue.push(m_root);
+        while (!queue.empty()) {
+            Node* curr_node = queue.front();
+            queue.pop();
+
+            output_order.push_back(curr_node->val);
+
+            // Enqueue the children of our current node if they exist
+            if (curr_node->left != nullptr) { queue.push(curr_node->left); }
+            if (curr_node->right != nullptr) { queue.push(curr_node->right); }
+        }
+
+        return output_order;
+    } 
 };
